ed467a7cd4391e5967a91d1329545ad6a1bf8bc5
AhmadSaleh22 <dev.ahmadsaleh@gmail.com>
2021-12-29 18:32:11 +0200

    upgrade value of imageInfo

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,16 @@ import { MainContainer } from './Component/widget/Section'
 import ErrorBoundary from './Component/ErrorBoundary/ErrorBoundary'
 import { Suspense } from 'react'
 import { SpinnerContainer } from './Component/widget/SpinnerContainer'
-import { NavLinkWrapper } from './Component/widget/Nav/NavLinkWrapper'
-import { Header } from './Component/Header/Header'
+// import { NavLinkWrapper } from './Component/widget/Nav/NavLinkWrapper'
+// import { Header } from './Component/Header/Header'
 import Loadable from './Hoc/Loadable'
 import { Image } from './Component/widget/Image'
 import logo from './unsplash.png'
+import { ImageInfo } from './Component/ImageInfo/ImageInfo'
 function App() {
   return (
     <Suspense fallback={<SpinnerContainer />}>
-        <Image src={logo} />
+        <ImageInfo />
     </Suspense>
 //     <Providers>
 //             <MainContainer>
--- src/Component/ImageInfo/ImageInfo.tsx
+++ src/Component/ImageInfo/ImageInfo.tsx
@@ -2,37 +2,44 @@ import { Row } from '../widget/Row'
 import Button from '@mui/material/Button'
 import ReplyIcon from '@mui/icons-material/Reply'
 import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline'
-import Popover from '@mui/icons-material/ErrorOutline'
-import Tooltip from '@mui/icons-material/Tooltip'
+import Tooltip from '@mui/material/Tooltip'
+import Popover from '@mui/material/Popover'
+
 import { Typography } from '../widget/Typography'
 
 export const ImageInfo = () => {
     return (
         <Row>
             <Row>
-                <Button variant = "outline" startIcon={ <ReplyIcon />}>Share</Button>
-                <Button variant = "outline" startIcon={ <ErrorOutlineIcon />}>Share</Button>
+            <Button variant='outlined' startIcon={<ReplyIcon />}>
+            Share
+            </Button>
+                <Button variant = 'outlined' startIcon={ <ErrorOutlineIcon />}>Share</Button>
                 <Popover
+                open={false}
                 anchorOrigin={{
                     vertical: 'top',
                     horizontal: 'center',
-                }}
-                transformOrigin={{
+                  }}
+                  transformOrigin={{
                     vertical: 'center',
                     horizontal: 'center',
-                }}
-                >
-            <Tooltip>
-                <Button color="error">
-                    Report
-                </Button>
-            </Tooltip>
-            </Popover>
+                  }}
+               >
+                   <Tooltip title={'Report'} arrow>
+                       <Button color='error'>Report</Button>
+                   </Tooltip>
+                </Popover>
             </Row>
             <Row>
-                <Row>
-
-                </Row>
+                <Typography>Views</Typography>
+                <Typography>Downloads</Typography>
+                <Typography>Featured in</Typography>
+            </Row>
+            <Row>
+                <Typography>--</Typography>
+                <Typography>240</Typography>
+                <Typography>Editorial</Typography>
             </Row>
         </Row>
     )
