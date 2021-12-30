98b3e308b3d31b93fae10a6e5f8b21a1e5e6ec57
Ahmed .M A. Ghanim <42157141+ham1o7d@users.noreply.github.com>
2021-12-30 18:39:41 +0200

    I finished a main and search layouts using Material-UI and Styled-Components, The layouts has good responcive design
    #9

---
+++ src/Layouts/MainLayout/MainLayout.tsx
@@ -0,0 +1,32 @@
+
+import { useMediaQuery } from '@mui/material';
+import { Image, Layout } from './styled';
+
+interface ImageType{
+    img: string;
+    title: string
+}
+interface Props{
+    images: ImageType[]
+}
+
+const MainLayout= ({images}: Props)=> {
+
+    const isMeduim= useMediaQuery("(max-width: 950px)");
+    const isSmall= useMediaQuery("(max-width: 650px)");
+
+    
+    return (
+          <Layout variant="masonry" cols={ isSmall ? 1 : isMeduim? 2 : 3 } gap={15}>
+            {images.map((item) => (
+                <Image
+                  src={`${item.img}?w=248&fit=crop&auto=format`}
+                  srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                  alt={item.title}
+                  loading="lazy"
+                />
+            ))}
+          </Layout>
+      );
+}
+export default MainLayout;
---
+++ src/Layouts/MainLayout/styled.ts
@@ -0,0 +1,15 @@
+import { ImageList} from "@mui/material";
+import styled from "styled-components";
+
+export const Layout= styled(ImageList)`
+    margin: 40px auto;
+    width: 80%;
+    height: 100%;
+`
+export const Image= styled.img`
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+    margin-bottom: 10px;
+`
+
