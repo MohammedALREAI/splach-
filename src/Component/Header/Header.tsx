6d4baf5c0e840097602b84d1c3bb93b48892aff2
AhmadSaleh22 <dev.ahmadsaleh@gmail.com>
2021-12-30 18:18:30 +0200

    adding and last commit of header

--- src/Component/Header/Header.tsx
+++ src/Component/Header/Header.tsx
@@ -1,15 +1,20 @@
-import { NavLinkA } from '../widget/Nav/NavLinkA'
-import { NavLinkWrapper } from '../widget/Nav/NavLinkWrapper'
+import { NavLinkA } from '../widget/Nav/NavLinkA';
+import { Logo, NavLinkWrapper } from '../widget/Nav/NavLinkWrapper';
+import logo from '../../logo.png';
+import AutoComplete from 'Component/AutoComplete';
 
 
 export const Header = ({}) => {
     return (
         <>
-        <NavLinkWrapper className= "NavLinkWrapper">
-            <NavLinkA >Ahmad Saleh</NavLinkA>
-            <NavLinkA >Ahmad Saleh</NavLinkA>
-            <NavLinkA >Ahmad Saleh</NavLinkA>
-        </NavLinkWrapper>
+        <NavLinkWrapper>
+      <NavLinkA>
+      <Logo src={logo} />
+      </NavLinkA>
+      <NavLinkA>
+        <AutoComplete width="1500px" bR="2" />
+      </NavLinkA>
+    </NavLinkWrapper>
         </>
-    )
-}
+    );
+};
